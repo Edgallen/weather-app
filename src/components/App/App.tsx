76c02b3988bb7fc73fd756bf15255f18d6a254d3
Edgallen <popov567@gmail.com>
2022-08-02 12:58:58 +0300

    scss

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 
 const App = () => {
   return (
     <div>
-      <h1 className='heading'>Hello world</h1>
+      <h1 className='heading'>Weather App</h1>
     </div>
   );
 };
