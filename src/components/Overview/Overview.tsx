a66bd95473509f178abea3bd139e80eac4f74c66
Edgallen <popov567@gmail.com>
2022-08-02 22:53:42 +0300

    overview

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -4,40 +4,79 @@ import './Overview.scss';
 const Overview = () => {
   return (
     <section className='overview'>
-      <div className=''>
-        <div>
-          <h2>Mejasem Barat</h2>
-          <p>Tegal, Indonesia</p>
+      <div className='location'>
+        <div className='location__container'>
+          <h2 className='location__title'>Mejasem Barat</h2>
+          <p className='location__subtitle'>Tegal, Indonesia</p>
         </div>
 
-        <h3>08:54 AM</h3>
+        <h3 className='location__time'>08:54 AM</h3>
       </div>
 
-      <div>
-        <div>
-          <i className="ri-sun-cloudy-fill"></i>
-          <h1>20° C</h1>
+      <div className='weather'>
+        <i className="ri-sun-cloudy-fill weather__icon"></i>
+
+        <div className='weather__container'>
+          <h1 className='weather__temp'>20° C</h1>
+          <h2 className='weather__status'>Dramatic Cloudy</h2>
         </div>
       </div>
 
-      <div>
-        <h2>Chance of rain</h2>
+      <div className='hourly'>
+        <h2 className='overview__title'>Chance of rain</h2>
 
-        <div>
-          <p>7 PM</p>
-          <div>
-            <div></div>
+        <div className='hourly__container'>
+          
+          <div className='hourly__card'>
+            <p className='hourly__time'>7 PM</p>
+            <div className='hourly__bar'>
+              <div className='hourly__progress'></div>
+            </div>
+            <p className='hourly__chance'>44%</p>
           </div>
-          <p>44%</p>
+
+          <div className='hourly__card'>
+            <p className='hourly__time'>7 PM</p>
+            <div className='hourly__bar'>
+              <div className='hourly__progress'></div>
+            </div>
+            <p className='hourly__chance'>44%</p>
+          </div>
+
+          <div className='hourly__card'>
+            <p className='hourly__time'>7 PM</p>
+            <div className='hourly__bar'>
+              <div className='hourly__progress'></div>
+            </div>
+            <p className='hourly__chance'>44%</p>
+          </div>
+
+          <div className='hourly__card'>
+            <p className='hourly__time'>7 PM</p>
+            <div className='hourly__bar'>
+              <div className='hourly__progress'></div>
+            </div>
+            <p className='hourly__chance'>44%</p>
+          </div>
         </div>
       </div>
 
-      <div>
-        <h2>Sunrise & Sunset</h2>
+      <div className='sun'>
+        <h2 className='overview__title'>Sunrise & Sunset</h2>
 
-        <div>
-          <div>
+        <div className='sun__container'>
+          <div className='sun__card'>
 
+            <div className='sun__bio'>
+              <i className="ri-sun-fill sunrise__icon"></i>
+
+              <div className='sun__description'>
+                <p className='sun__text'>Sunrise</p>
+                <h2 className='sun__title'>4:20 AM</h2>
+              </div>
+            </div>
+
+            <p className='sun__count'>4 hours ago</p>
           </div>
         </div>
       </div>
