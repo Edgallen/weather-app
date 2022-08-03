48d214c3fe07a61c1d88415e38af20b1cb404b11
Edgallen <popov567@gmail.com>
2022-08-03 14:32:27 +0300

    todayPanel

--- src/components/TodayPanel/TodayPanel.tsx
+++ src/components/TodayPanel/TodayPanel.tsx
@@ -3,7 +3,77 @@ import styles from './TodayPanel.module.scss';
 
 const TodayPanel = () => {
   return (
-    <div>TodayPanel</div>
+    <section className={styles.todayPanel}>
+      <h2 className={styles.todayPanel__title}>Today overview</h2>
+
+      <div className={styles.cards__container}>
+        {/* Карточка */}
+        <div className={styles.card}>
+          <div className={styles.status__container}>
+            <i className={`ri-windy-line ${styles.status__icon}`}></i>
+
+            <div className={styles.status__description}>
+              <h3 className={styles.status__title}>Wind Speed</h3>
+              <h1 className={styles.status__info}>12km/h</h1>
+            </div>
+          </div>
+
+          <div className={styles.changes__container}>
+            <i className={`ri-arrow-up-s-fill ${styles.changes__icon} ${styles.icon__increase}`}></i>
+            <p className={styles.changes__text}>2km/h</p>
+          </div>
+        </div>
+        {/* Карточка */}
+        <div className={styles.card}>
+          <div className={styles.status__container}>
+            <i className={`ri-windy-line ${styles.status__icon}`}></i>
+
+            <div className={styles.status__description}>
+              <h3 className={styles.status__title}>Wind Speed</h3>
+              <h1 className={styles.status__info}>12km/h</h1>
+            </div>
+          </div>
+
+          <div className={styles.changes__container}>
+            <i className={`ri-arrow-up-s-fill ${styles.changes__icon} ${styles.icon__decrease}`}></i>
+            <p className={styles.changes__text}>2km/h</p>
+          </div>
+        </div>
+        {/* Карточка */}
+        <div className={styles.card}>
+          <div className={styles.status__container}>
+            <i className={`ri-windy-line ${styles.status__icon}`}></i>
+
+            <div className={styles.status__description}>
+              <h3 className={styles.status__title}>Wind Speed</h3>
+              <h1 className={styles.status__info}>12km/h</h1>
+            </div>
+          </div>
+
+          <div className={styles.changes__container}>
+            <i className={`ri-arrow-up-s-fill ${styles.changes__icon} ${styles.icon__decrease}`}></i>
+            <p className={styles.changes__text}>2km/h</p>
+          </div>
+        </div>
+          {/* Карточка */}
+          <div className={styles.card}>
+            <div className={styles.status__container}>
+              <i className={`ri-windy-line ${styles.status__icon}`}></i>
+
+              <div className={styles.status__description}>
+                <h3 className={styles.status__title}>Wind Speed</h3>
+                <h1 className={styles.status__info}>12km/h</h1>
+              </div>
+            </div>
+
+            <div className={styles.changes__container}>
+              <i className={`ri-arrow-up-s-fill ${styles.changes__icon} ${styles.icon__increase}`}></i>
+              <p className={styles.changes__text}>2km/h</p>
+            </div>
+          </div>
+
+      </div>
+    </section>
   )
 }
 
